4f0293ef52c3c94e0d05916b02c24d684fcff59c
zexnego-cmd <zex_nego@hotmail.com>
2025-08-19 20:08:22 -0300

    Termino

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-let getRankTT = soma(90, 50)
-let value = (90 - 50)
-
+soma(200, 148)
 
 function soma(n1, n2){
     let rank = n1 - n2
-    if (rank <= 10){
-    return "Ferro"
-}else if (rank <= 20){
-    return "Bronze"
-}else if (rank <= 50){
-    return "Prata"
-}else if (rank <= 70){
-    return "Ouro"
-}else if (rank <= 80){
-    return "Platina"
-}else if (rank <= 90){
-    return "Ascendente"
-}else if (rank <= 100){
-    return "Imortal"
-}else {
-    return "Radiante"
-}
+        if (rank <= 10){
+        let nivelXp = "Ferro"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }else if (rank <= 20){
+            nivelXp = "Bronze"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }else if (rank <= 50){
+            nivelXp ="Prata"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }else if (rank <= 80){
+            nivelXp = "Ouro"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }else if (rank <= 90){
+            nivelXp = "Diamante"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }else if (rank <= 100){
+            nivelXp = "Lendario"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }else {
+            nivelXp = "Imortal"
+            console.log("O Herói tem de saldo de " + rank + " está no nível de " + nivelXp)
+        }
 }
 
-console.log ("O Herói tem de saldo de " + value + " está no nível de " + getRankTT)
